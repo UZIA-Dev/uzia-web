59c3a8ee3bdafa864a960ad8f6f1729034619758
Kumampet <kumampet@gmail.com>
2024-12-15 15:24:10 +0900

    ロゴ横に文字追加

--- src/components/AppBar/parts/LogoIcon.tsx
+++ src/components/AppBar/parts/LogoIcon.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
-import basePath from '../../../utils/getBasePath'
+import Typography from '@mui/material/Typography';
 
 type LogoIconProps = {
   href?: string;
@@ -11,8 +11,9 @@ type LogoIconProps = {
 const LogoIcon = (props: LogoIconProps) => {
   const { href = "/", className } = props;
   return (
-    <Box sx={{ display: { xs: 'flex' }, mr: 1 }}>
-      <Link className={`logo-icon-conteiner ${className}`} href={href} underline="none"></Link>
+    <Box sx={{ display: { xs: 'flex' }, mr: 1, flexDirection: "row", alignItems: "center" }}>
+      <Link className={`logo-icon-conteiner ${className}`} href={href} underline="none" />
+      <Typography noWrap variant="subtitle2" sx={{ fontWeight: 100 }}>OFFICIAL WEBSITE</Typography>
     </Box>
   );
 }
