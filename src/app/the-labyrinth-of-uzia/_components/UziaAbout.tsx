b89f06c38986447744c10fcb9aaafd66ed0ac888
Kumampet <kumampet@gmail.com>
2024-12-16 01:13:51 +0900

    ユーザイアの迷宮の名称修正。ルーティング変更

--- src/app/the-labyrinth-of-uzia/_components/UziaAbout.tsx
+++ src/app/the-labyrinth-of-uzia/_components/UziaAbout.tsx
@@ -7,7 +7,7 @@ import Introduction from './about/introduction.json';
 const UziaAbout = () => {
   return (
     <Box className="uzui-content-container" sx={{ flexGrow: 1 }}>
-      <Typography variant='h2' className="container-title">UZIAについて</Typography>
+      <Typography variant='h2' className="container-title">「ユーザイアの迷宮」について</Typography>
       <Typography variant="body1" sx={{ whiteSpace: "pre-wrap", textAlign: "center" }}>{Introduction.introduction}</Typography>
     </Box>
   );
