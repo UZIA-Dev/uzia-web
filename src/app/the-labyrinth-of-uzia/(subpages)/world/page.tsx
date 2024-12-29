4694661968718b72947e983f2156625c30296ff4
Kumampet <kumampet@gmail.com>
2024-12-30 04:08:30 +0900

    Worldサブページ土台作成

---
+++ src/app/the-labyrinth-of-uzia/(subpages)/world/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UziaSubPageWorld = () => {
+  return (
+    <div>
+      <h2>World</h2>
+      {/* About Us Content */}
+    </div>
+  );
+}
+
+export default UziaSubPageWorld;
